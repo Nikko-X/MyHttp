1806541bb5d3d545e01bc6052b846d29fc55eb91
Nikko-X <15956472745@163.com>
2022-10-11 22:19:12 +0800

    添加解析客户端请求

--- MyInterpd/MyInterpd/htpp.cpp
+++ MyInterpd/MyInterpd/htpp.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 // 网络通信需要包含的头文件、以及加载的库文件
 #include <WinSock2.h>
 #pragma comment(lib, "WS2_32.lib")
+
+// 定义一个宏(相比与定义函数，宏节省开销)
+// 可打印出当前函数，所在行，以及需打印的字符
+#define PRINT(str)	printf("[%s - %d]"#str"=%s\n", __func__, __LINE__, str);
 
 void error_die(const char* str) {
 	perror(str);
@@ -70,14 +75,85 @@ int startup(unsigned short* port) {
 	return server_socket;
 
 }
+
+// 从指定的客户端套接字，读取一行数据，保存到buff中
+// 返回实际读取的字节数
+int get_line(int sock, char* buff, int size) {
+	int i = 0;
+	char c = 0; // '\0'
+
+	while (i< size-1 && c != '\n') {
+		int n = recv(sock, &c, 1, 0);
+		if (n > 0) {
+			if (c == '\r') {		// 一行结尾可能是/r /n
+				n = recv(sock, &c, 1, MSG_PEEK);		// 查看下一个字符但不取出
+				if (n > 0 && c == '\n') {
+					recv(sock, &c, 1, 0);	// 正式读取
+				}
+				else {
+					c = '\n';
+				}
+			}
+			buff[i++] = c;
+		}
+		else {
+			c = '/n';	// 读取失败,相当于break
+		}
+	}
+	buff[i] = 0;
+	return i;
+}
+
+void unimplement(int client) {
+	// 向指定套接字，发送一个错误提示，显示不支持该功能
+
+}
+
 //处理用户请求的线程函数
 DWORD WINAPI accept_request(LPVOID arg) {
+	char buff[1024];
+	int client = (SOCKET)arg;
+	// 读取一行数据
+	int numchars =  get_line(client, buff, sizeof(buff));
+	PRINT(buff);
+	char method[255];
+	int j = 0, i= 0;
+	while (!isspace(buff[j]) && i < sizeof(method) - 1) {
+		method[i++] = buff[j++];
+	}
+	method[i] = 0;	// '\0'
+	PRINT(method);
+
+	//检查请求的方法，本服务器是否支持
+	if (stricmp(method, "GET") && strcmp(method, "POST")) {
+		// 向浏览器返回一个错误提示
+		unimplement(client);
+		return 0;
+	}
+	
+	// 解析资源文件路径
+
+	char url[255]; // 存放请求的资源的完整路径
+	i = 0;
+	// 跳过空格
+	while (isspace(buff[j]) && j < sizeof(buff)) j++;
+
+	while (!isspace(buff[j]) && i < sizeof(url) - 1 && j< sizeof(buff)) {
+		url[i++] = buff[j++];
+	}
+	url[i] = 0;
+	PRINT(url);
+
+	// www.xh.com
+	//127.0.0.1 这些是根目录
+	// url	/
+	// htdocs/
 
 	return 0;
 }
 
 int main(void) {
-	unsigned short port = 0;
+	unsigned short port = 80;
 	int server_sock= startup(&port);
 	printf("httpd服务已经启动，正在监听 %d 端口", port);
 
